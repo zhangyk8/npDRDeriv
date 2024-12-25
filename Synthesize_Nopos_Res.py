5924f69e1f03e95746e043b32e05d74e9899fa0d
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2024-12-24 21:17:17 -0800

    Add files via upload

--- Synthesize_Nopos_Res.py
+++ Synthesize_Nopos_Res.py
@@ -42,7 +42,7 @@ for fac in fac_lst:
     theta_C_DR1_lst = [] 
     theta_C_sd_DR1_lst = []
     for b in range(1, B+1):
-        with open('./Results/Sim1_Nopos_theta_'+str(b)+'_h'+str(fac)+'_selfnorm3.dat', "rb") as file:
+        with open('./Results/Sim1_Nopos_theta_'+str(b)+'_h'+str(fac)+'_inner_selfnorm3.dat', "rb") as file:
             theta_ra5, theta_ra1, theta_ipw5, theta_ipw1, theta_dr5, theta_sd5, theta_dr1, theta_sd1,\
             theta_C_RA5, theta_C_RA1, theta_C_IPW5, theta_C_IPW1, theta_C_DR5, theta_C_sd5,\
             theta_C_DR1, theta_C_sd1 = pickle.load(file)
@@ -71,7 +71,7 @@ for fac in fac_lst:
         theta_C_DR1_lst.append(theta_C_DR1)
         theta_C_sd_DR1_lst.append(theta_C_sd1)
         
-    with open('./Syn_Results/Sim1_Nopos_theta_h'+str(fac)+'_selfnorm3.dat', "wb") as file:
+    with open('./Syn_Results/Sim1_Nopos_theta_h'+str(fac)+'_inner_selfnorm3.dat', "wb") as file:
         pickle.dump([theta_ra5_lst, theta_ra1_lst, theta_ipw5_lst, theta_ipw1_lst,
                      theta_dr5_lst, theta_sd_dr5_lst, theta_dr1_lst, theta_sd_dr1_lst,
                      theta_C_RA5_lst, theta_C_RA1_lst, theta_C_IPW5_lst, theta_C_IPW1_lst,
